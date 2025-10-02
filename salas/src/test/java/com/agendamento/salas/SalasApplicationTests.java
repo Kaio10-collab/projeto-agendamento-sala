07072a4d0ce6822760adf87082a6b3eaa96fc952
kaio10-collab <sancheskaio@gmail.com>
2025-10-02 15:49:11 -0300

    primeiro commit do meu projeto.

---
+++ salas/src/test/java/com/agendamento/salas/SalasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendamento.salas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
